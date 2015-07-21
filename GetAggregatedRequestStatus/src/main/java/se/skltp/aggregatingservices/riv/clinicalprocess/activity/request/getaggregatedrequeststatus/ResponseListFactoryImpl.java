f79a6251ae44a4b6fa1115e051059e48319952f5
Martin Flower <martin.flower@callistaenterprise.se>
2015-07-21 14:32:58 +0200

    SERVICE-232 - result element

--- GetAggregatedRequestStatus/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/activity/request/getaggregatedrequeststatus/ResponseListFactoryImpl.java
+++ GetAggregatedRequestStatus/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/activity/request/getaggregatedrequeststatus/ResponseListFactoryImpl.java
@@ -8,33 +8,36 @@ import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
 
 import riv.clinicalprocess.activity.request.getrequeststatusresponder.v1.GetRequestStatusResponseType;
 import riv.clinicalprocess.activity.request.getrequeststatusresponder.v1.ObjectFactory;
+import riv.clinicalprocess.activity.request.v1.ResultCodeEnum;
+import riv.clinicalprocess.activity.request.v1.ResultType;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
 import se.skltp.agp.service.api.QueryObject;
 import se.skltp.agp.service.api.ResponseListFactory;
 
 public class ResponseListFactoryImpl implements ResponseListFactory {
 
-	private static final Logger log = LoggerFactory.getLogger(ResponseListFactoryImpl.class);
-	private static final JaxbUtil jaxbUtil = new JaxbUtil(GetRequestStatusResponseType.class, ProcessingStatusType.class);
-	private static final ObjectFactory OF = new ObjectFactory();
-	
-	@Override
-	public String getXmlFromAggregatedResponse(QueryObject queryObject, List<Object> aggregatedResponseList) {
-		GetRequestStatusResponseType aggregatedResponse = new GetRequestStatusResponseType();
+    private static final Logger log = LoggerFactory.getLogger(ResponseListFactoryImpl.class);
+    private static final JaxbUtil jaxbUtil = new JaxbUtil(GetRequestStatusResponseType.class, ProcessingStatusType.class);
+    private static final ObjectFactory OF = new ObjectFactory();
 
-	    for (Object object : aggregatedResponseList) {
-	    	GetRequestStatusResponseType response = (GetRequestStatusResponseType)object;
-			aggregatedResponse.getRequestStatus().addAll(response.getRequestStatus());
-		}
+    @Override
+    public String getXmlFromAggregatedResponse(QueryObject queryObject, List<Object> aggregatedResponseList) {
+        GetRequestStatusResponseType aggregatedResponse = new GetRequestStatusResponseType();
 
-	    if (log.isInfoEnabled()) {
-    		String subjectOfCareId = queryObject.getFindContent().getRegisteredResidentIdentification();
-        	log.info("Returning {} aggregated remisstatus for subject of care id {}", aggregatedResponse.getRequestStatus().size() ,subjectOfCareId);
+        for (Object object : aggregatedResponseList) {
+            GetRequestStatusResponseType response = (GetRequestStatusResponseType) object;
+            aggregatedResponse.getRequestStatus().addAll(response.getRequestStatus());
         }
-
+        
+        aggregatedResponse.setResult(new ResultType());
+        aggregatedResponse.getResult().setLogId("NA");
+        aggregatedResponse.getResult().setResultCode(ResultCodeEnum.INFO);
+        
+        String subjectOfCareId = queryObject.getFindContent().getRegisteredResidentIdentification();
+        log.info("Returning {} aggregated remisstatus for subject of care id {}", aggregatedResponse.getRequestStatus().size(), subjectOfCareId);
 
         // Since the class GetRequestStatusResponseType doesn't have an @XmlRootElement annotation
         // we need to use the ObjectFactory to add it.
         return jaxbUtil.marshal(OF.createGetRequestStatusResponse(aggregatedResponse));
-	}
+    }
 }
